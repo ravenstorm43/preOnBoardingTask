bd67f815f2c29cf71d8f0695415f6200883086ca
kyungtae42 <kimbam1017@naver.com>
2024-09-02 16:07:26 +0900

    initialize

---
+++ src/test/java/com/sparta/preonboardingtask/PreOnBoardingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.preonboardingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreOnBoardingTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
